b94e227bcf59169605a834c207dca4f8d1f11589
Jay Wilson Jr <jwilson@ndgcommunications.com>
2017-08-31 23:52:45 -0400

    add authintication methods

--- src/providers/auth/auth.ts
+++ src/providers/auth/auth.ts
@@ -1,18 +1,46 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import 'rxjs/add/operator/map';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase } from 'angularfire2/database';
+import * as firebase from 'firebase';
 
-/*
-  Generated class for the AuthProvider provider.
-
-  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
-  for more info on providers and Angular DI.
-*/
 @Injectable()
 export class AuthProvider {
 
-  constructor(public http: Http) {
+  constructor(public afAuth: AngularFireAuth, public afDatabase: AngularFireDatabase) {
     console.log('Hello AuthProvider Provider');
+  }
+
+  getUser(): firebase.User {
+    return this.afAuth.auth.currentUser;
+  }
+
+  loginUser(newEmail: string, newPassword: string): firebase.Promise<any> {
+    return this.afAuth.auth.signInWithEmailAndPassword(newEmail, newPassword);
+  }
+
+  anonymousLogin(): firebase.Promise<any> {
+    return this.afAuth.auth.signInAnonymously();
+  }
+
+  linkAccount(email: string, password: string): firebase.Promise<any> {
+    const credential = firebase.auth.EmailAuthProvider.credential(email, password);
+
+    return this.afAuth.auth.currentUser.linkWithCredential(credential)
+      .then(user => {
+        this.afDatabase.object(`/userProfile/${user.uid}/`).update({
+          email: email
+        });
+      }, error => {
+        console.error('There was an error linking the account', error);
+      });
+  }
+
+  resetPassword(email): firebase.Promise<any> {
+    return this.afAuth.auth.sendPasswordResetEmail(email);
+  }
+
+  logoutUser(): firebase.Promise<void> {
+    return this.afAuth.auth.signOut();
   }
 
 }
